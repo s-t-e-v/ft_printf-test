a81d64385969145011bf45a681a78c48de941626
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-03 17:23:47 +0100

    up

--- test_printf.c
+++ test_printf.c
@@ -7,15 +7,15 @@ int	main(void)
 	char	*str;
 
 	str = NULL;
-	// // NULL str test
-	// n = printf(str); // -1
-	// printf("\n%d\n", n); // nothing happen
-	// // unique %
-	// n = printf("%"); // -1
-	// printf("\n%d\n", n); // nothing happen
-	// // unique % with char(s) before
-	// n = printf("f%"); // -1
-	// printf("\n%d\n", n); // f
+	// NULL str test
+	n = printf(str); // -1
+	printf("\n%d\n", n); // nothing happen
+	// unique %
+	n = printf("%"); // -1
+	printf("\n%d\n", n); // nothing happen
+	// unique % with char(s) before
+	n = printf("f%"); // -1
+	printf("\n%d\n", n); // f
 	// unique % with char(s) before
 	n = printf("====%=%"); // 7
 	printf("\n%d\n", n); // ====%=% (as it is)
